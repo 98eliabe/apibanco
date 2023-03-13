cb974f30ddea4c82ff88aae421e5d79816e8cfe6
kaficher <kamillaficher@gmail.com>
2023-03-13 14:51:16 -0300

    cliente model

---
+++ src/main/java/br/com/projetofinalturmaiv/apibanco/model/Cliente.java
@@ -0,0 +1,59 @@
+package br.com.projetofinalturmaiv.apibanco.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table
+public class Cliente {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	int id;
+
+	@Column(name = "nome_cliente")
+	String nome;
+
+	@Column(name = "cpf_cliente")
+	String cpf;
+
+	@Column(name = "telefone_cliente")
+	String telefone;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+
+}
