5088bfd7e82b66f7ac22504dbf6a8d4df422171c
kaficher <kamillaficher@gmail.com>
2023-03-13 16:48:21 -0300

    implementacao do service do movimentacao

--- src/main/java/br/com/projetofinalturmaiv/apibanco/service/MovimentacaoServiceImpl.java
+++ src/main/java/br/com/projetofinalturmaiv/apibanco/service/MovimentacaoServiceImpl.java
@@ -1,5 +1,25 @@
 package br.com.projetofinalturmaiv.apibanco.service;
 
-public class MovimentacaoServiceImpl {
+import java.util.ArrayList;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import br.com.projetofinalturmaiv.apibanco.model.Movimentacao;
+
+public class MovimentacaoServiceImpl implements IMovimentacaoService {
+	@Autowired
+	private MovimentacaoRepo repo;
+
+	@Override
+	public ArrayList<Movimentacao> recuperarTodas() {
+		// TODO Auto-generated method stub
+		return (ArrayList<Movimentacao>) repo.findAll();
+	}
+
+	@Override
+	public Movimentacao cadastrarNova(Movimentacao nova) {
+		// TODO Auto-generated method stub
+		return repo.save(nova);
+	}
 
 }
