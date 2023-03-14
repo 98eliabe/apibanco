515d75ce38be759fed9b3f99c4796ed341ad1939
Eliabe Nascimento <eliabe041298@gmail.com>
2023-03-14 09:47:15 -0300

    Att de classes

--- src/main/java/br/com/projetofinalturmaiv/apibanco/repo/ContaRepo.java
+++ src/main/java/br/com/projetofinalturmaiv/apibanco/repo/ContaRepo.java
@@ -4,14 +4,11 @@ import java.util.ArrayList;
 
 import org.springframework.data.repository.CrudRepository;
 
+import br.com.projetofinalturmaiv.apibanco.model.Cliente;
 import br.com.projetofinalturmaiv.apibanco.model.Conta;
 
 public interface ContaRepo extends CrudRepository<Conta, Integer> {
 	
-	public ArrayList<Conta> findAll();
-
-	public ArrayList<Conta> findByOrderByCliente();
-	
-	public ArrayList<Conta> findByOrderById();
-	
+	public ArrayList<Conta> findByCliente(Cliente cliente);
+		
 }
--- src/main/java/br/com/projetofinalturmaiv/apibanco/repo/MovimentacaoRepo.java
+++ src/main/java/br/com/projetofinalturmaiv/apibanco/repo/MovimentacaoRepo.java
@@ -4,10 +4,11 @@ import java.util.ArrayList;
 
 import org.springframework.data.repository.CrudRepository;
 
+import br.com.projetofinalturmaiv.apibanco.model.Conta;
 import br.com.projetofinalturmaiv.apibanco.model.Movimentacao;
 
 public interface MovimentacaoRepo extends CrudRepository<Movimentacao, Integer>{
 	
-	public ArrayList<Movimentacao> findAll();
+	public ArrayList<Movimentacao> findByConta(Conta conta);
 
 }
